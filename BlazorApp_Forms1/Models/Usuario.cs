7be86e0f616c95a6ab012cfe74f4f61858f8ea8e
Peterson Scaramussi <peterson.silva6@fatec.sp.gov.br>
2024-03-26 09:08:55 -0300

    add Blazor Form

---
+++ BlazorApp_Forms1/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp_Forms1.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="O nome deve ser informado")]
+        [StringLength(10, ErrorMessage ="O nome não pode exceder 10 caracteres")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O email deve ser informado")]
+        [EmailAddress(ErrorMessage ="Formato do email é inválido")]
+        public string Email { get; set; }
+        [Range(18,80, ErrorMessage = "A idade deve estar entre 18 e 80")]
+        public int Idade { get; set; }
+    }
+}
